00349caa996cc0f51587890834a226a8ee0ee1cc
R <1610816530@qq.com>
2020-10-24 17:48:30 +0800

    111

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,6 @@
 $(function () {
     //开发服务器地址
-    var baseURl = 'http://ajax.frontend.itheima.net';
+    var baseURl = 'http://127.0.0.1:3007';
     //测试环境服务器地址
     // var baseURl = 'http://ajax.frontend.itheima.net';
     //生产环境服务器地址
